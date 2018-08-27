d01b35599dad30d288792882ebb38954d6d806c4
dhpark <dhpark0648@gmail.com>
2018-08-27 21:50:37 +0900

    유저 페이지 업데이트

--- public/client/users/controllers/users.client.controller.js
+++ public/client/users/controllers/users.client.controller.js
@@ -5,8 +5,6 @@ angular.module('users').controller('UsersController', [
     $scope.user = Authentication
     $scope.scores = []
     $scope.score = ''
-    $scope.total = $scope.user ? $scope.user.scores.total : 0
-    $scope.count = $scope.user ? $scope.user.scores.count : 0
     $scope.userId = $routeParams.userId
 
     // 자기 자신의 정보로 들어오면 users 페이지로 이동시킨다
@@ -15,19 +13,17 @@ angular.module('users').controller('UsersController', [
     }
 
     // 임의의 사용자가 다른 사용자의 정보를 보는 경우
-    if ($routeParams.userId && (!$scope.user || $scope.user._id != $routeParams.userId)) {
-      $http({
-        method: 'GET',
-        url: '/api/users/' + $routeParams.userId
-      }).then((response) => {
-        // console.log('After find user: ', response)
-        $scope.total = response.data.scores.total
-        $scope.count = response.data.scores.count
-      }, (errorResponse) => {
-        // console.log('After find user(error): ', errorResponse)
-        $scope.error = errorResponse.data.message
-      })
-    }
+    // if ($routeParams.userId && (!$scope.user || $scope.user._id != $routeParams.userId)) {
+    //   $http({
+    //     method: 'GET',
+    //     url: '/api/users/' + $routeParams.userId
+    //   }).then((response) => {
+    //     // console.log('After find user: ', response)
+    //   }, (errorResponse) => {
+    //     // console.log('After find user(error): ', errorResponse)
+    //     $scope.error = errorResponse.data.message
+    //   })
+    // }
 
     $scope.find = () => {
       Scores.query({
@@ -56,8 +52,6 @@ angular.module('users').controller('UsersController', [
         user: $routeParams.userId,
       }, (response) => {
         // console.log('After registering scores: ', response)
-        $scope.total += response.score
-        $scope.count++
         $scope.scores.total += response.score
         $scope.scores.unshift(response)
       }, (errorResponse) => {
@@ -76,8 +70,6 @@ angular.module('users').controller('UsersController', [
               $scope.scores.splice(i, 1)
             }
           }
-          $scope.count--
-          $scope.total -= score.score
           $scope.scores.total -= score.score
         })
       }
